7d88f0c3a05c95cec3421a17b29c07e1ea30231d
vitorlazarotto <vitor.lazarotto@equiplano.com.br>
2021-06-01 11:09:04 -0300

    Resource de produtos

---
+++ backend/src/main/java/com/devsuperior/dscatalog/services/ProductService.java
@@ -0,0 +1,102 @@
+package com.devsuperior.dscatalog.services;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Product;
+import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.DatabaseException;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional(readOnly = true) // ** importar do spring / readOnly melhora a performance não dando lock no bd
+	public List<ProductDTO> findAll() {
+		List<Product> list = repository.findAll();
+		List<ProductDTO> dtos = 
+				list.stream().map(cat -> new ProductDTO(cat))
+					.collect(Collectors.toList());
+		return dtos;
+	}
+	
+	public Page<ProductDTO> findAllPaged(PageRequest pageRequest) {
+		
+		Page<Product> list = repository.findAll(pageRequest);
+		
+		Page<ProductDTO> dtos = 
+				list.map(cat -> new ProductDTO(cat));
+		
+		return dtos;
+	}
+
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Optional<Product> prodOpt = repository.findById(id);
+		Product entity = prodOpt.orElseThrow(() -> new EntityNotFoundException("Produto não encontrado"));
+		System.out.println(entity.getCategories().size());
+		return new ProductDTO(
+					entity,				
+					entity.getCategories()
+				);
+	}
+
+
+	@Transactional
+	public ProductDTO salvar(ProductDTO dto) {
+		Product prod = new Product();
+		//prod.setName(dto.getName());
+		prod = repository.save(prod);
+		
+		ProductDTO dtoSalvo = new ProductDTO(prod);
+		
+		return dtoSalvo;
+	}
+
+
+	@Transactional
+	public ProductDTO atualizar(Long id, ProductDTO dto) {
+		
+		try {
+			Product prod = repository.getById(id);
+			//prod.setName(dto.getName());
+			prod = repository.save(prod);
+			return new ProductDTO(prod);
+		}
+		catch(EntityNotFoundException ex) {
+			throw new ResourceNotFoundException("Id not found "+ id);
+		}
+		
+	}
+
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch(EmptyResultDataAccessException ex) {
+			throw new ResourceNotFoundException("Id not found "+ id);
+		}
+		catch(DataIntegrityViolationException dataEx) {
+			throw new DatabaseException("Recurso não pode ser excluído pois possui registros dependentes");
+		}
+	}
+
+
+
+}
